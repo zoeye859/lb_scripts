dc04b217013a04e0fdee562a565472570ff310a3
Zoe Ye <haoyang.ye@cantab.net>
2022-03-16 18:44:09 +0100

    Update max_min_extract.py

--- max_min_extract.py
+++ max_min_extract.py
@@ -13,9 +13,11 @@ import math
 
 
 def max_min_val(file_name):
+    print ('Opening file ', file_name)
     hdul = fits.open(file_name)
     data = hdul[0].data
     val = np.abs(data.max()/data.min())
+    hdul.close()
     return val
 
 def collect_val(dir_num, PATH, d):
@@ -25,8 +27,8 @@ def collect_val(dir_num, PATH, d):
     d['dir_num'][int(dir_num)] = int(dir_num)
     for filename in os.listdir(PATH):
         if filename.startswith(dir_num+'_') and filename.endswith('.fits'):
-            val = max_min_val(filename)
-            idx = Path(filename).stem.split('_')[1]
+            val = max_min_val(PATH + filename)
+            idx = Path(filename).stem.split('_')[-1]
             d[idx][int(dir_num)] = val
             print (filename, idx, val)
     return d
@@ -37,11 +39,9 @@ def init_d(keylist,dir_sum):
         d[i] = [math.nan]*(dir_sum+1)
     return d
 
-#PATH = '/net/rijn2/data2/Haoyang/ALICE/selfcal_1b1/fits_collection_by_number'
-PATH = str(Path().absolute()).split("\/")[0] # Directory of current working directory
-save_PATH = PATH + '/plot_png/'
-markers = ['o', 'v', '^', '<', '>', 's', 'p', 'P', '*', 'X', 'D']
 keylist = ['dir_num', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+PATH = str(Path().absolute()).split("\/")[0] + '/fits_collection/' # Directory of current working directory
+save_PATH = str(Path().absolute()).split("\/")[0] + '/data_analysis/'
 dir_sum = 100
 
 isExist = os.path.exists(save_PATH)
@@ -50,8 +50,6 @@ if not isExist:
     os.makedirs(save_PATH)
     print("The new directory noise_png is created!")
 
-
-
 d = init_d(keylist, dir_sum)
 for i in range(1,dir_sum+1):
     print (i)
@@ -59,6 +57,6 @@ for i in range(1,dir_sum+1):
 
 df = pd.DataFrame(dict([ (k,pd.Series(v)) for k,v in d.items() ]))
 
-df.to_hdf('max_min_fits.h5', key='selfcal_1b1', mode='w')
+df.to_hdf(save_PATH + 'max_min_fits.h5', key='selfcal_1b1', mode='w')
 
-df.to_csv (r'max_min_fits.csv', index = False, header=True)
+df.to_csv (save_PATH + r'max_min_fits.csv', index = False, header=True)
